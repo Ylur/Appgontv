dd91c138f38f37c6684ffc1cf805aaa076ad6a73
Ylur <158998739+Ylur@users.noreply.github.com>
2024-05-12 21:39:11 +0000

    Add files via upload

--- Locations/MapView.swift
+++ Locations/MapView.swift
@@ -3,19 +3,24 @@
 //  Appgo
 //
 //  Created by Ingi Haraldss on 5.5.2024.
-//
 
 import Foundation
 import SwiftUI
 import MapKit
 
 struct MapView: View {
-    var coordinate: CLLocationCoordinate2D
+    
     @State private var region: MKCoordinateRegion
+    var coordinate: CLLocationCoordinate2D
 
     init(coordinate: CLLocationCoordinate2D) {
         self.coordinate = coordinate
-        _region = State(initialValue: MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)))
+        _region = State(initialValue: MKCoordinateRegion(
+            center: coordinate, 
+            span: MKCoordinateSpan(
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01)
+        ))
     }
 
     var body: some View {
