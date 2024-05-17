4d8649e075d186c1dbc4ae607597e110d82dcf26
Ylur <158998739+Ylur@users.noreply.github.com>
2024-05-17 20:01:42 +0000

    Firebase tenging

--- AppgoApp.swift
+++ AppgoApp.swift
@@ -1,4 +1,9 @@
+//  AppgoApp.swift
+//  Appgo
 //
+//  Created by Ingi Haraldss on 5.5.2024.
+//
+
 //  AppgoApp.swift
 //  Appgo
 //
@@ -6,9 +11,29 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
-struct YourApp: App {
+struct Appgo: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        // passa að firestore komi eftir firebaseapp config
+        DispatchQueue.main.async {
+            let settings = FirestoreSettings()
+            Firestore.firestore().settings = settings
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -16,5 +41,3 @@ struct YourApp: App {
         }
     }
 }
-
-
